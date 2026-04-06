e6cb2d40cde12eb6d21ce6e38cae1e8e217f0e83
Rashidbek <rasishidbekrasuljonov83>
2026-04-06 15:12:38 +0300

    type-conversion-js

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 //foydalanuvchi ikki xonalik son kiritadi undan 10 liklar honasidagi sonni oling console.log() ga chiqaring
 
+// 1-masala
+
 // let A = 55;
 // let birlar = A % 10;
 // let unlar = (A - birlar) / 10;
 // console.log(unlar);
+
+//============================================================================================================================
+
+// 4-masala
+
+// Toq yoki juft (faqat operator bilan)   let n = 7;   вќ“ Shart: рџ‘‰   if ishlatmasdan:   "toq"   "juft"   chiqaring
+
+// let son = 20;
+// let natijasi = (son % 2 === 0 && "Juft") || "Toq";
+// console.log(natijasi);
