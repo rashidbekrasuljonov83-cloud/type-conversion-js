775de63bfd9354c6a872c7eac5e05f5e2e165500
Rashidbek <rasishidbekrasuljonov83>
2026-04-06 14:46:33 +0300

    type-conversion-js

---
+++ main.js
@@ -0,0 +1,6 @@
+//foydalanuvchi ikki xonalik son kiritadi undan 10 liklar honasidagi sonni oling console.log() ga chiqaring
+
+// let A = 55;
+// let birlar = A % 10;
+// let unlar = (A - birlar) / 10;
+// console.log(unlar);
